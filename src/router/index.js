fe1490d7094e43552ad1e236de7582c11cfbb863
Sumotra <sumotra@mail.ru>
2019-10-19 00:40:36 +0700

    completed step 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Auth/Login'
+import Registration from '@/components/Auth/Registration'
+import Uimini from 'uimini/dist/css/uimini.min.css'
 
 Vue.use(Router)
+Vue.use(Uimini)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: Registration
+    }
+  ]
 })
